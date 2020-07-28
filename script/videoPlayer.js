83d53274e9f87b7949f4ee2b3ec31cd0d4b61c97
Tatiana <lts221@yandex.ru>
2020-07-28 16:07:30 +0300

    Написание видеоплеера

--- script/videoPlayer.js
+++ script/videoPlayer.js
@@ -1,3 +1,79 @@
 export const videoPlayerInit = () => {
-    console.log('Video Init');
+    console.log('Video Player');
+
+    // video-player
+    // video-button__play
+    // video-button__stop
+    // video-time__passed
+    // video-progress
+    // video-time__total
+    const videoPlayer = document.querySelector('.video-player');
+    const videoTimePassed = document.querySelector('.video-time__passed');
+    const videoButtonPlay = document.querySelector('.video-button__play');
+    const videoProgress = document.querySelector('.video-progress');
+    const videoTimeTotal = document.querySelector('.video-time__total');
+    const videoButtonStop = document.querySelector('.video-button__stop');
+
+    const toggleIcon = () => {
+        if (videoPlayer.paused) {
+            videoButtonPlay.classList.remove('fa-pause');
+            videoButtonPlay.classList.add('fa-play');
+        } else {
+            videoButtonPlay.classList.add('fa-pause');
+            videoButtonPlay.classList.remove('fa-play');
+        }
+    }
+
+    const togglePlay = () => {
+
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
+    };
+
+    const stopPlay = () => {
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+    }
+
+    //   Функция добавляения нуля перед минутами и секундами в плейере
+    const addZero = n => n < 10 ? '0' + n : n;
+
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', toggleIcon);
+    videoPlayer.addEventListener('pause', toggleIcon);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+    videoPlayer.addEventListener('timeupdate', () => {
+        const curentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
+
+        videoProgress.value = (curentTime / duration) * 100; // движение прогресс бара, в зависимости от проигрывания видео
+
+        let minutePassed = Math.floor(curentTime / 60);  // выделяем минуты, округляем
+        let secondsPassed = Math.floor(curentTime % 60);  // получаем секунды из остатка минут
+
+
+        let minuteTotal = Math.floor(duration / 60); // выделяем минуты, округляем
+        let secondsTotal = Math.floor(duration % 60); // получаем секунды из остатка минут
+
+        //   Добавляем отображение минут и секунд на страницу
+        // videoTimePassed.textContent = addZero(minutePassed) + ':' + addZero(secondsPassed); можно переписать на:
+        videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+        //   videoTimeTotal.textContent = addZero(minuteTotal) + ':' + addZero(secondsTotal); можно переписать на:
+        videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+
+    });
+
+    videoProgress.addEventListener('change', () => {
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
+
+        videoPlayer.currentTime = (value * duration) / 100; // присваиваем новое время, куда мы должны переключить
+    });
 };
